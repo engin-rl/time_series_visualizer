6fceabf43beb6a1f536c25ed3312367523730511
engin-rl <engincelikk04@gmail.com>
2024-08-30 14:55:14 +0300

    not finished yet

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -6,29 +6,63 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv")
+df = df.set_index("date").reset_index()
+
+df["date"] = pd.to_datetime(df["date"])
+print(df.head())
+
 
 # Clean data
-df = None
+
+df = df[
+    (
+        (df["value"] >= df["value"].quantile(0.025))
+        & (df["value"] <= df["value"].quantile(0.975))
+    )
+]
+
+print(df.count())
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(16, 5))
+    ax.plot(df["date"], df["value"], linestyle="-", color="r")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
 
     # Save image and return fig (don't change this part)
     fig.savefig("line_plot.png")
+    plt.show()
     return fig
 
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
+    df["monthly_average"] = df["date"].dt.month
+
+    print(df)
+
+    # print(df["date"].dt.year)
 
     # Draw bar plot
+    """fig, ax = plt.subplots(figsize=(16, 5))
+    ax.plot(df["date"], df["value"], linestyle="-", color="r")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")"""
 
     # Save image and return fig (don't change this part)
-    fig.savefig("bar_plot.png")
-    return fig
+    # fig.savefig("bar_plot.png")
+    # return fig
+
+
+draw_bar_plot()
 
 
 def draw_box_plot():
