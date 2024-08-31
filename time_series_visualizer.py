6f2415084210e85145ed079e4233a22e18420073
engin-rl <engincelikk04@gmail.com>
2024-08-31 11:00:11 +0300

    progressed a lil bit

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -42,24 +42,49 @@ def draw_line_plot():
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
 
+    # df["month"] = df["date"].dt.month
+    # df["monthly_average"] = df["date"].dt.month
+
     df["year"] = df["date"].dt.year
     df["month"] = df["date"].dt.month
-    df["monthly_average"] = df["date"].dt.month
+    print(df.head())
+    """df_melted_year = pd.melt(
+        df,
+        id_vars=["year"],
+        value_vars=["value"],  # Specify the column(s) to melt
+        var_name="variable",  # Name for the 'variable' column in the melted DataFrame
+        value_name="value_melted",  # Use a different name that doesn't conflict
+    )"""
 
-    print(df)
+    df_2 = df.drop(columns="date")
+    df_melted = pd.melt(
+        df_2,
+        id_vars=["year", "month"],
+        value_name="values",
+    )
 
-    # print(df["date"].dt.year)
+    print(df_melted)
+
+    df_melted_grouped = (
+        df_melted.groupby(["year", "month"])
+        .agg({"values": "mean"})
+        .reset_index()
+        .astype(int)
+    )
+    print(df_melted_grouped)
 
     # Draw bar plot
-    """fig, ax = plt.subplots(figsize=(16, 5))
-    ax.plot(df["date"], df["value"], linestyle="-", color="r")
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Page Views")
-    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")"""
+    fig = plt.subplots(figsize=(10, 10))
+    plt.bar(data=df_melted_grouped, x="year", height="values")
+    plt.xlabel("Years")
+    plt.ylabel("Average Page Views")
+
+    plt.legend()
+    plt.show()
 
     # Save image and return fig (don't change this part)
     # fig.savefig("bar_plot.png")
-    # return fig
+    return fig
 
 
 draw_bar_plot()
